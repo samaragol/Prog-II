549577ef7ce367f60c4d96823aff5a090a89865b
Samara Gonçalves <samaragonoliveira@gmail.com>
2025-10-25 23:25:50 -0300

    bibli 03 prontão

---
+++ exercícios/03_bibliotecas/bibli_03/Resultados/SamaraGoncalves/completo/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "data.h"
+
+int main()
+{
+    int dia1, mes1, ano1, dia2, mes2, ano2;
+
+    scanf("%d/%d/%d", &dia1, &mes1, &ano1);
+    scanf("%d/%d/%d", &dia2, &mes2, &ano2);
+
+    if (!verificaDataValida(dia1, mes1, ano1) || !verificaDataValida(dia2, mes2, ano2))
+    {
+        printf("A primeira e/ou segunda data(s) invalida(s)\n");
+        return 0;
+    }
+
+    printf("Primeira data: ");
+    imprimeDataExtenso(dia1, mes1, ano1);
+    printf("Segunda data: ");
+    imprimeDataExtenso(dia2, mes2, ano2);
+
+    if (comparaData(dia1, mes1, ano1, dia2, mes2, ano2) == -1)
+    {
+        printf("A primeira data eh mais antiga\n");
+    }
+    else if (comparaData(dia1, mes1, ano1, dia2, mes2, ano2) == 1)
+    {
+        printf("A segunda data eh mais antiga\n");
+    }
+    else
+    {
+        printf("As datas sao iguais\n");
+    }
+
+    printf("A diferenca em dias entre as datas eh: %02d dias\n", calculaDiferencaDias(dia1, mes1, ano1, dia2, mes2, ano2));
+
+    return 0;
+}
