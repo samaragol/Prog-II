09029d96baa5a7b3c01e722126f339a369b6643f
Samara Gonçalves <samaragonoliveira@gmail.com>
2025-11-30 00:28:04 -0300

    aloc_02 feito de verdade agora

--- exercícios/06_alocacao_dinamica/01_geral/aloc_02/Respostas/SamaraGoncalves/main.c
+++ exercícios/06_alocacao_dinamica/01_geral/aloc_02/Respostas/SamaraGoncalves/main.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utils.h"
+
+int main()
+{
+    int **matriz, linhas, colunas;
+
+    scanf("%d %d\n", &linhas, &colunas);
+
+    matriz = CriaMatriz(linhas, colunas);
+    LeMatriz(matriz, linhas, colunas);
+    ImprimeMatrizTransposta(matriz, linhas, colunas);
+    LiberaMatriz(matriz, linhas);
+
+    return 0;
+}
